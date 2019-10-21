8a831fd073232f8250f51b1d24a48775aba17c6f
Glitch (working-heaven) <none>
2019-10-21 18:48:01 +0000

    👠👑 Updated From shawn removed music command

--- server.js
+++ server.js
@@ -143,7 +143,6 @@ bot.on("message", message => {
         .setTitle("Help Commands")
         .setColor("RANDOM")
         .addField(".mod", "Shows the mod help commands", true)
-        .addField(".music", "Shows the music commands", true)
         .addField(".fun", "Shows the fun commands", true)
       .addField('.botinfo', "tell you the bot info", true)
       .addField("Support", "[Support Server](https://discord.gg/pyYqvnd)", true)
@@ -198,82 +197,12 @@ bot.on("message", message => {
       message.delete().catch();
       message.channel.send(botmessage);
       break;
-    case "music":
-      let musicembed = new Discord.RichEmbed()
-        .setTitle("Music Commands")
-        .setColor("RANDOM")
-        .addField(".play", "Only url works", true)
-        .addField(".stop", "Stops the songs", true)
-        .addField(".skip", "Skips the song", true);
-      message.channel.send(musicembed);
-       message.delete()
-      break;
-    case "p":
-      function play(connection, message) {
-        var server = servers[message.guild.id];
-
-        server.dispatcher = connection.playStream(
-          ytdl(server.queue[0], { filter: "audioonly" })
-        );
-
-        server.queue.shift();
-
-        server.dispatcher.on("end", function() {
-          if (server.queue[0]) {
-            play(connection, message);
-          } else {
-            connection.disconnect();
-          }
-        });
-      }
-
-      if (!args[1]) {
-        message.channel.send("You need to provide a link");
-        return;
-      }
-      if (!message.member.voiceChannel) {
-        message.channel.send("You need to be in a voice channe to play music");
-        return;
-      }
-      if (!servers[message.guild.id])
-        servers[message.guild.id] = {
-          queue: []
-        };
-
-      var server = servers[message.guild.id];
-
-      server.queue.push(args[1]);
-
-      if (!message.guild.voiceConnection)
-        message.member.voiceChannel.join().then(function(connection) {
-          play(connection, message);
-        });
-      message.delete();
-
-      break;
     case 'stopbot':
      message.reply('Bot has been stopped or restarted if you use a process manager)')
 		.then(() => {
 			console.log(`Bot stopped by ${message.author.tag} (${message.author.id})`);
 			process.exit(0);
 		});
-      break;
-    case "s":
-      var server = servers[message.guild.id];
-      if (server.dispatcher) server.dispatcher.end();
-      message.channel.send("I haved skiped the song");
-      break;
-    case "stop":
-      var server = servers[message.guild.id];
-      if (message.guild.voiceConnection) {
-        for (var i = server.queue.length - 1; i >= 0; i--) {
-          server.queue.splice(i, 1);
-        }
-        server.dispatcher.end();
-        message.channel.send("Ending the queue and leaving the voice channel");
-        console.log("Stopped the queue");
-      }
-      if (message.guild.connection) message.guild.voiceConnection.disconnect();
       break;
     case "mute":
       let person = message.guild.member(
@@ -305,31 +234,6 @@ bot.on("message", message => {
         person.removeRole(muterole.id);
         message.channel.send(`@${person.user.tag} has been unmuted!`);
       }, ms(time));
-
-      break;
-        case "verify":
-      var vyuser = message.author.username;
-      //user msg
-      let vyusermsg = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setFooter(`hi ${vyuser}`)
-        .setThumbnail(`https://verifyservices.net/wp-content/uploads/2018/05/black-registered-logo_verify-cropped.png`)
-        .setTimestamp()
-        .setDescription("You got verifyed");
-
-      message.member.send(vyusermsg);
-
-      message.channel.send(`hi ${vyuser} welcome to the support server`);
-
-      message.member.addRole("634561295290466314");
-
-      var logsChannel = message.guild.channels.find("name", "logs");
-      if (!logsChannel)
-        return message.channel.send("Admin or owner make a logs channel");
-
-      logsChannel.send(vyusermsg);
-
-      message.delete();
 
       break;
   }
